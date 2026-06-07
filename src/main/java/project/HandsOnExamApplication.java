5baa437005a8b58c36f61d8774fa66f792396bc7
Davi Ribeiro <daviribeirogomes123@gmail.com>
2026-06-07 12:42:30 -0300

    Initial Spring Boot project

---
+++ src/main/java/project/HandsOnExamApplication.java
@@ -0,0 +1,13 @@
+package project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsOnExamApplication.class, args);
+	}
+
+}
